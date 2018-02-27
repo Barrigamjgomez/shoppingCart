ea4385dc6977f08bb5a19f652c34ce3a9b4baada
Meliza-fb <maite.e.figue@hotmail.com>
2018-02-27 11:44:04 -0300

    callback

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
-'use strict'
+$(document).ready(function(){
+
+let image= [];
+let name= [];
+let description=[]
+let precio=[]
+  for (let i = 0; i < shirtMarket.length; i++) {
+    name.push(shirtMarket[i].name);
+    $('.thumbnail').append(`<h5>${name}</h5>`)
+  }
+  for (var i = 0; i < shirtMarket.length; i++) {
+    image.push(shirtMarket[i].image);
+    $('.thumbnail').append(`<img src="${image}" alt="...">`);
+  }
+  for (var i = 0; i < shirtMarket.length; i++) {
+    description.push(shirtMarket[i].description);
+    $('.thumbnail').append(`<p>${description}</p>`);
+  }
+  for (var i = 0; i < shirtMarket.length; i++) {
+    precio.push(shirtMarket[i].Precio);
+    $('.thumbnail').append(`<span>${precio}</span>`)
+  }
+
+});
