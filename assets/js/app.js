f7341d74f8c9bd62d41cfc876b4538522e5edd08
Meliza-fb <maite.e.figue@hotmail.com>
2018-02-28 22:00:58 -0300

    bajando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
-   /*========
-   Data.json
-
-
   for (let i = 0; i < shirtMarket.length; i++) {
-    shirtMarket[i];
-    $('.thumbnails').append(`<h5> + ${shirtMarket[i]} +  </h5><img src="${image}" alt="..."><p>${description}</p><span>${precio}</span>`)
+
+      $('.muestrate').append('<div class="contenedor"><img class="imagen" src="' + shirtMarket[i].image
+        +'"><div class="caption row"><div class="col-sm-12"><h5>'
+        + shirtMarket[i].name+ '</h5><p>' + shirtMarket[i].description
+        + '</p><span>' + shirtMarket[i].precio +'</span></div></div>'
+        //parte boton
+        + '<div class="row"><div class="col-sm-5 "><div class="paypal">'
+        + '<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">'
+        + '<input type="hidden" name="cmd" value="_s-xclick">'
+        + '<input type="hidden" name="hosted_button_id" value="5A34WFDB5F8G4">'
+        + '<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">'
+        + '<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">'
+        + '</form></div></div></div></div></div></div></div>')
+
   }
-     ==========*/
 
 
 // Get the modal1
